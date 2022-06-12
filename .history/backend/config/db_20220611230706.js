55537c9ae45d11aceac103cfe3b02914967b5624
Odell Purifoy <opurifoy@springhealth.com>
2022-06-11 23:13:19 -0400

    Added connection to Mongo database

---
+++ .history/backend/config/db_20220611230706.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Mongo DB connected: ${conn.connection.host}` .cyan.underline)
+    } catch (error) {
+        console.log(error)
+        process.exit()
+    }
+}
